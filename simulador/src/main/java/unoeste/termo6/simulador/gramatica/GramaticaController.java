fab7192139e2ceee6daf3a4b14b0ccba0ff78337
Joao-Paulo-Bortolocce <bbortolocce@gmail.com>
2025-09-18 22:18:32 -0300

    finalizando geração de palavras

--- simulador/src/main/java/unoeste/termo6/simulador/gramatica/GramaticaController.java
+++ simulador/src/main/java/unoeste/termo6/simulador/gramatica/GramaticaController.java
@@ -6,14 +6,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -48,10 +46,13 @@ public class GramaticaController implements Initializable {
 
     private static int id;
 
+    private ArrayList<String> alfabeto;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         variaveis = new ArrayList<>();
         id = 0;
+        alfabeto = new ArrayList<>();
         resultsPane.setVisible(false);
         resultsPane.setManaged(false);
 
@@ -84,8 +85,9 @@ public class GramaticaController implements Initializable {
 
         variableField.textProperty().addListener((observable, oldValue, newValue) -> {
             int pos;
-            for (pos=0;pos<variaveis.size() &&  variaveis.get(pos).getId().compareToIgnoreCase(variableId)!=0;pos++);
-            if(pos<variaveis.size()){
+            for (pos = 0; pos < variaveis.size() && variaveis.get(pos).getId().compareToIgnoreCase(variableId) != 0; pos++)
+                ;
+            if (pos < variaveis.size()) {
                 variaveis.get(pos).setNome(newValue);
             }
 
@@ -114,13 +116,13 @@ public class GramaticaController implements Initializable {
         HBox transitionRow = new HBox(5);
         transitionRow.setAlignment(Pos.CENTER_LEFT);
 
-        No no = new No("",null,null);
+        No no = new No("", null, null);
         int pos;
 
-        for (pos=0;pos<variaveis.size() && variaveis.get(pos).getId().compareToIgnoreCase(variableId)!=0;pos++);
+        for (pos = 0; pos < variaveis.size() && variaveis.get(pos).getId().compareToIgnoreCase(variableId) != 0; pos++)
+            ;
 
-        if(pos<variaveis.size())
-        {
+        if (pos < variaveis.size()) {
             variaveis.get(pos).getTransicao().insere(no);
         }
 
@@ -143,24 +145,44 @@ public class GramaticaController implements Initializable {
         return transitionRow;
     }
 
-    public boolean aceita(String palavra){
+    private void addAlfabeto(String newValue) {
+        if(newValue!=""){
+
+            int i;
+            for (i = 0; i < alfabeto.size() && newValue.compareToIgnoreCase(alfabeto.get(i)) > 0; i++) ;
+            if (i == alfabeto.size() || alfabeto.get(i).compareToIgnoreCase(newValue )!=0) {
+                alfabeto.add(i,newValue);
+            }
+        }
+
+    }
+
+    public boolean aceita(String palavra) {
         //deve começar com a variavel inicial que o user escolheu
         String var = variavelInicial.getText();
-        if (!var.isEmpty()){
+        if (!var.isEmpty()) {
             int i;
             for (i = 0; i < variaveis.size() && variaveis.get(i).getNome().compareToIgnoreCase(var) != 0; i++) ;
 
-            if (i==variaveis.size())
+            if (i == variaveis.size())
                 return false;
 
             Variavel variavel = variaveis.get(i);
+            if (palavra.length() == 0) {
+                No aux = variavel.getTransicao().getCabeca();
+                while (aux != null && aux.getLeitura().compareTo("") != 0)
+                    aux = aux.getProx();
+                if (aux != null && ( aux.getTransicao() == null  || aux.getTransicao().compareTo("") == 0))
+                    return true;
+                return false;
+            }
 
             for (i = 0; i < palavra.length(); i++) {
                 char letra = palavra.charAt(i);
                 No aux = variavel.getTransicao().getCabeca();
                 //verificar se na lista adjacente tem essa letra, se tiver muda a variavel atual para a transicao, se nao tiver retorna falso
                 boolean encontrou = false;
-                while (aux!=null && !encontrou){
+                while (aux != null && !encontrou) {
                     if (!aux.getLeitura().isEmpty()) {
                         if (aux.getLeitura().charAt(0) == letra) {
                             encontrou = true;
@@ -169,21 +191,27 @@ public class GramaticaController implements Initializable {
                             //verificar se não é a ultima letra da palavra a ser lida e se essa transicao for nula entao deve aprovar também
                             if (i == palavra.length() - 1 && (transicao == null || transicao.isEmpty()))
                                 return true;
-
-                            for (i = 0; i < variaveis.size() && variaveis.get(i).getNome().compareToIgnoreCase(var) != 0; i++)
-                                ;
-
-                            if (i == variaveis.size())
+                            if (i < palavra.length() - 1 && (transicao == null || transicao.isEmpty()))
                                 return false;
 
-                            variavel = variaveis.get(i);
+//                            for (i = 0; i < variaveis.size() && variaveis.get(i).getNome().compareToIgnoreCase(var) != 0; i++)
+                            int j;
+                            for (j = 0; j < variaveis.size() && variaveis.get(j).getNome().compareToIgnoreCase(transicao) != 0; j++)
+                                ;
+
+                            if (j == variaveis.size())
+                                return false;
+
+                            variavel = variaveis.get(j);
+
                         }
                     }
-                    aux=aux.getProx();
+                    aux = aux.getProx();
                 }
                 if (!encontrou)
                     return false;
             }
+            return false;
         }
 
         return true;
@@ -192,41 +220,128 @@ public class GramaticaController implements Initializable {
 
     @FXML
     void verifyWord(ActionEvent event) {
-        showResultsPane();
-        verificationResultBox.setVisible(true);
-        verificationResultBox.setManaged(true);
-
-        String palavra = wordField.getText();
-        if (palavra.isEmpty()) {
-            resultLabel.setText("Por favor, digite uma palavra.");
-            resultLabel.setStyle("-fx-text-fill: #FFC107;");
-            return;
+        if (variavelInicial.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Atenção");
+            alert.setHeaderText(null); // deixa sem cabeçalho
+            alert.setContentText("Não é possível verificar uma palavra sem uma variável inicial");
+            alert.showAndWait();
         }
+        else{
 
-        boolean aceita= aceita(palavra);
+            showResultsPane();
+            verificationResultBox.setVisible(true);
+            verificationResultBox.setManaged(true);
 
-        if (aceita) {
-            resultLabel.setText("A palavra '" + palavra + "' é ACEITA!");
-            resultLabel.setStyle("-fx-text-fill: #8BC34A; -fx-font-weight: bold;"); // Verde para sucesso
-        } else {
-            resultLabel.setText("A palavra '" + palavra + "' NÃO é aceita.");
-            resultLabel.setStyle("-fx-text-fill: #F44336; -fx-font-weight: bold;"); // Vermelho para falha
+            String palavra = wordField.getText();
+            if (palavra.isEmpty()) {
+                resultLabel.setText("Por favor, digite uma palavra.");
+                resultLabel.setStyle("-fx-text-fill: #FFC107;");
+            }
+            else{
+                int j;
+                for (j = 0; j < variaveis.size() && variaveis.get(j).getNome().compareToIgnoreCase(variavelInicial.getText()) != 0; j++);
+
+                if (j == variaveis.size()){
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Atenção");
+                    alert.setHeaderText(null); // deixa sem cabeçalho
+                    alert.setContentText("Informe uma variavel que exista, "+variavelInicial.getText()+" não é uma variavel da gramatica");
+                    alert.showAndWait();
+                }
+                else{
+
+
+
+                    boolean aceita = aceita(palavra);
+
+                    if (aceita) {
+                        resultLabel.setText("A palavra '" + palavra + "' é ACEITA!");
+                        resultLabel.setStyle("-fx-text-fill: #8BC34A; -fx-font-weight: bold;"); // Verde para sucesso
+                    } else {
+                        resultLabel.setText("A palavra '" + palavra + "' NÃO é aceita.");
+                        resultLabel.setStyle("-fx-text-fill: #F44336; -fx-font-weight: bold;"); // Vermelho para falha
+                    }
+                }
+            }
+        }
+    }
+
+    private void gerarAlfabeto() {
+        for (int i = 0; i < variaveis.size(); i++) {
+            No aux= variaveis.get(i).getTransicao().getCabeca();
+            while(aux!=null ){
+                addAlfabeto(aux.getLeitura());
+                aux=aux.getProx();
+            }
         }
     }
 
     @FXML
     void generateWords(ActionEvent event) {
-        showResultsPane();
-        generationResultBox.setVisible(true);
-        generationResultBox.setManaged(true);
-        generatedWordsListView.getItems().clear();
+        if (variavelInicial.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Atenção");
+            alert.setHeaderText(null); // deixa sem cabeçalho
+            alert.setContentText("Não é possível gerar palavras sem uma variável inicial");
+            alert.showAndWait();
+        }
+        else{
+            int k;
+            for (k = 0; k < variaveis.size() && variaveis.get(k).getNome().compareToIgnoreCase(variavelInicial.getText()) != 0; k++);
 
-        ArrayList<String> palavrasGeradas = new ArrayList<>();
-        //gerar as palavras
-        generatedWordsListView.setItems(FXCollections.observableArrayList(palavrasGeradas));
+            if (k == variaveis.size()){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Atenção");
+                alert.setHeaderText(null); // deixa sem cabeçalho
+                alert.setContentText("Informe uma variavel que exista, "+variavelInicial.getText()+" não é uma variavel da gramatica");
+                alert.showAndWait();
+            }
+            else{
+
+                gerarAlfabeto();
+                showResultsPane();
+                generationResultBox.setVisible(true);
+                generationResultBox.setManaged(true);
+                generatedWordsListView.getItems().clear();
+
+
+                ArrayList<String> palavrasGeradas = new ArrayList<>();
+                //gerar as palavrasGeradas
+
+                int cont = 0, ini = 0;
+                String palavra;
+                ArrayList<String> testadas = new ArrayList<>();
+                testadas.add("");
+                boolean flag = true;
+                if (aceita("")) {
+                    palavrasGeradas.add("");
+                    cont++;
+                }
+                while (cont < 10 && flag) {
+                    for (; flag && ini < testadas.size() && cont < 10; ini++) {
+                        for (int j = 0; flag && j < alfabeto.size() && cont < 10; j++) {
+                            palavra = testadas.get(ini) + alfabeto.get(j);
+                            testadas.add(palavra);
+                            if (aceita(palavra)) {
+                                palavrasGeradas.add(palavra);
+                                cont++;
+                            } else if (palavra.length() > 10) {
+                                flag = false;
+                            }
+                            System.out.println(palavra);
+                        }
+                    }
+                }
+                if (palavrasGeradas.get(0).compareTo("") == 0) {
+                    palavrasGeradas.set(0, "ε");
+                }
+                generatedWordsListView.setItems(FXCollections.observableArrayList(palavrasGeradas));
+            }
+        }
     }
 
-    private void Show (){
+    private void Show() {
         for (Variavel variavel : variaveis) {
             System.out.print(variavel.getNome() + " -> ");
             No aux = variavel.getTransicao().getCabeca();
